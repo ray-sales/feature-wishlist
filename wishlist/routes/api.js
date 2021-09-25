084fc0ab42c5b3dc70966135e01981e49b882d34
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-09-25 19:01:59 -0300

    create wishlist methods
    create wishlist methods

--- wishlist/routes/api.js
+++ wishlist/routes/api.js
@@ -1,1 +1,24 @@
-var cookieParser = require('cookie-parser');
+var express = require('express');
+
+var router = express.Router();
+
+router.post("/add-wishlist", (req, res) => {
+    let product = req.body.id;
+    let products = [];
+    if (req.cookies && req.cookies.products)
+        products = JSON.parse(req.cookies.products);
+    products.push(product);
+    res.cookie("products", JSON.stringify(products));
+    res.json("OK");
+})
+
+router.post("/remove-wishlist", (req, res) => {
+    let id = req.body.id;
+    let products = JSON.parse(req.cookies.products);
+    let index = products.indexOf(id);
+    products.splice(index, 1);
+    res.cookie("products", JSON.stringify(products));
+    res.json("OK");
+})
+
+module.exports = router;
--- wishlist/routes/index.js
+++ wishlist/routes/index.js
@@ -4,10 +4,9 @@ const axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
     axios.get('https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e')
         .then(data => {
-            console.log(data.data.products)
             res.render('index', {
                 title: 'Home',
                 breadcump: 'Home',
@@ -17,8 +16,26 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.get('/wishlist', (req, res) => {
-    res.render('wishlist')
+router.get("/wishlist", (req, res) => {
+    if (req.cookies.products)
+        axios.get('https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e')
+        .then(data => {
+            let products = JSON.parse(req.cookies.products).map(e => {
+                console.log(e);
+                return data.data.products.find(x => x.id == e);
+            });
+            res.render('index', {
+                title: 'Wishlist',
+                breadcump: 'Home > Wishlist',
+                products: products
+            })
+        })
+    else
+        res.render('index', {
+            title: 'Wishlist',
+            breadcump: 'Home > Wishlist',
+            products: []
+        })
 })
 
 module.exports = router;
